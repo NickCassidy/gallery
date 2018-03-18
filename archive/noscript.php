63a80eff0fb74f38fb827f4266af744ff543f8a4
NickCassidy <nick@achromat.net>
2018-03-18 12:40:29 +0000

    tidy css and amends to noscript views and incomplete work to add tabindex

--- archive/noscript.php
+++ archive/noscript.php
@@ -1,47 +1,54 @@
 <?php
+
 ######################################################
-# Project:		SimpleViewer SEO Gallery Plugin		 #
-# Version:		2.02								 #
-# Author:		Mikael Kjellstrom					 #
 # License:		Copyright (c) 2010 Mikael Kjellstrom #
 ######################################################
 
- $imgpath = 'https://www.achromat.net/simon_new/web';  // The path to the gallery 
- $altmaxchars = "50";   // The maximum characters of the ALT tag taken from the caption 
+    $imgpath = 'https://www.achromat.net/simon_new/web';  // The path to the gallery 
+
+    $altmaxchars = "50";   // The maximum characters of the ALT tag taken from the caption 
  
- $thisDirectory = basename(__DIR__);
+    $thisDirectory = basename(__DIR__);
 
-  $doc = new DOMDocument();
-  $doc->load( 'config.xml' );
-  
-  $gallery = $doc->getElementsByTagName( "simpleviewergallery" );
-  
-  $images = $doc->getElementsByTagName( "image" );
-	
-  print "<div = id=\"noscriptImageAlignment\">";
+    $doc = new DOMDocument();
 
-  foreach( $images as $image )
-  {
- 	 
-	 $largephoto = $image->getAttribute('imageURL');	 
+    $doc->load( 'config.xml' );
+  
+    $gallery = $doc->getElementsByTagName("simpleviewergallery");
+ 
+    $images = $doc->getElementsByTagName("image");
+
+print "<div = id=\"noscriptImageAlignment\">";
+
+    foreach( $images as $image )
+    {
+     
+        $largephoto = $image->getAttribute('imageURL');	 
 	 
-	 // strip slash and unwanted preceding dots from path   
- 	 $largephoto = substr($largephoto, 3);   
+        // strip slash and unwanted preceding dots from path   
+        $largephoto = substr($largephoto, 3);   
    
-   
+        $captions = $image->getElementsByTagName("caption");
 
-   $captions = $image->getElementsByTagName( "caption" );
- 	 $caption = $captions->item(0)->nodeValue;
+        $caption = $captions->item(0)->nodeValue;
   
-		$altcaption = substr($caption, 0, $altmaxchars);
-      $pos = strrpos($altcaption, " ");
-      if ($pos>0) {
-       $altcaption = substr($altcaption, 0, $pos);
-      }
+        $altcaption = substr($caption, 0, $altmaxchars);
 
-	  print "<img src=\"$imgpath/$largephoto\" alt=\"$altcaption\" class=\"noscriptImages\"\"/><p class=\"noscriptCaptions\"\">$caption</p>\n";
+        $pos = strrpos($altcaption, " ");    
 
-   }
-    print "</div>";
+    if ($pos>0) {
+
+            $altcaption = substr($altcaption, 0, $pos);
+
+            }
+    for ($i=0; $i<10; $i++) 
+        {       
+
+print "<img src=\"$imgpath/$largephoto\" alt=\"$altcaption\" class=\"noscriptImages\"  tabindex=\"$i\"  \"/><p class=\"noscriptCaptions\"\">$caption</p>\n";
+
+        }
+
+    }
+print "</div>";
 
 ?>
